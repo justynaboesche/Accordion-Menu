ef2870bb54e77c82c02bf15577805e05e6da905d
Justyna Boesche <justyna.boesche@gmail.com>
2024-03-14 11:56:06 +0100

    function openAccordionItems created

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const accordion = document.querySelector('.accordion');
 const accordionBtns = document.querySelectorAll('.accordion-btn');
 
+function openAccordionItems() {
+
+   this.nextElementSibling.classList.toggle('active');
+}
+
+
+
+accordionBtns.forEach(btn => btn.addEventListener('click', openAccordionItems));
