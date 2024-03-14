5d2245f3ca6b73b26adddb97b1ebdc4e0dc826e8
Justyna Boesche <justyna.boesche@gmail.com>
2024-03-14 12:19:36 +0100

    function closeAccordionItems created

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ const accordion = document.querySelector('.accordion');
 const accordionBtns = document.querySelectorAll('.accordion-btn');
 
 function openAccordionItems() {
-
-   this.nextElementSibling.classList.toggle('active');
+	if (this.nextElementSibling.classList.contains('active')) {
+		this.nextElementSibling.classList.remove('active');
+	} else {
+		closeAccordionItems();
+		this.nextElementSibling.classList.toggle('active');
+	}
 }
 
-
+const closeAccordionItems = () => {
+	const allActiveItems = document.querySelectorAll('.accordion-info');
+	allActiveItems.forEach(item => item.classList.remove('active'));
+};
 
 accordionBtns.forEach(btn => btn.addEventListener('click', openAccordionItems));
